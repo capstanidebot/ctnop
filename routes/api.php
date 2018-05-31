e12bce364977d62098c632284ce9a69e03166cbf
capstanidebot <capstanidebot@gmail.com>
2018-05-31 04:35:16 +0000

    update

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,10 @@
 
 Route::get('/test', function () {
     return ['test'];
+Route::get('/test', function () {
+    return ['test'];
+});
+Route::get('/test2', function () {
+    return ['test2'];
+});
 });
